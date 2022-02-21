34cff49abf12ec2debb5eb7de25c362edc1f75d8
Bierloga <90850242+Bierloga@users.noreply.github.com>
2022-02-21 22:16:57 +0100

    Add files via upload

---
+++ superpy/graphs.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+
+def get_graph(x_list, y_list, type):
+    plt.bar(x_list, y_list, zorder=3)
+    plt.xticks(rotation=90)
+    plt.xlabel("Days")
+    plt.ylabel(type)
+    plt.title(f"{type} over last week")
+    plt.tight_layout()
+    plt.grid(zorder=0)
+    plt.savefig(f"{type}_graph.png")
+    plt.show()
